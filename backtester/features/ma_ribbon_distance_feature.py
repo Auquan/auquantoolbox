94280f6fd3df9dae9f0e387d016b5fc639a9df41
Kanav Arora <kanav@stuph.co>
2017-09-23 02:58:01 +0530

    MARD feature

--- backtester/features/ma_ribbon_distance_feature.py
+++ backtester/features/ma_ribbon_distance_feature.py
@@ -2,6 +2,7 @@ from backtester.features.feature import Feature
 import numpy as np
 from scipy import stats
 from scipy import spatial
+import pandas as pd
 
 
 class MARibbonDistanceFeature(Feature):
@@ -13,26 +14,30 @@ class MARibbonDistanceFeature(Feature):
     def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
         instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
         dataDf = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['featureName'])
-        # TODO KANAV
-        raise NotImplementedError
+        instrumentDict = instrumentManager.getAllInstrumentsByInstrumentId()
+        instrumentIds = instrumentDict.keys()
+        rolling_means = pd.Series([np.zeros(featureParams['numRibbons'])] * len(instrumentDict), index=instrumentIds)
+
+        space = int((featureParams['endPeriod'] - featureParams['startPeriod']) / (featureParams['numRibbons'] - 1))
+
+        collatedData = {}
+        for idx in np.linspace(featureParams['startPeriod'], featureParams['endPeriod'], featureParams['numRibbons']):
+            i = int(idx)
+            collatedData[i] = dataDf[-i:].mean()
+            for instrumentId in instrumentIds:
+                rolling_means[instrumentId][i / space - 1] = collatedData[i][instrumentId]
+
+        toRtn = {}
+        for instrumentId in instrumentIds:
+            ranking = stats.rankdata(rolling_means[instrumentId])
+            d = spatial.distance.hamming(ranking, range(1, 1 + len(rolling_means[instrumentId])))
+            toRtn[instrumentId] = d
+
+        return pd.Series(toRtn, index=instrumentIds)
 
     @classmethod
     def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
         lookbackDataDf = instrumentManager.getDataDf()
-        data = lookbackDataDf[featureParams['featureName']]
-        rolling_means = np.zeros(featureParams['numRibbons'])
-        if len(data) < 1:
-            return rolling_means
-        space = int((featureParams['endPeriod'] - featureParams['startPeriod']) / (featureParams['numRibbons'] - 1))
-        for idx in np.linspace(featureParams['startPeriod'], featureParams['endPeriod'], featureParams['numRibbons']):
-            i = int(idx)
-            rolling_means[i / space - 1] = data[-i:].mean()
-        ranking = stats.rankdata(rolling_means)
-        d = spatial.distance.hamming(ranking, range(1, 1 + len(rolling_means)))
-        return d
-
-    @classmethod
-    def computeForLookbackData(cls, featureParams, featureKey, currentFeatures, lookbackDataDf):
         data = lookbackDataDf[featureParams['featureName']]
         rolling_means = np.zeros(featureParams['numRibbons'])
         if len(data) < 1:
