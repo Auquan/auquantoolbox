09a86cfeef1a7ec19509150a1231e78e08738855
Kanav Arora <kanav@stuph.co>
2017-09-23 03:47:24 +0530

    ratio feature

--- backtester/features/ratio_feature.py
+++ backtester/features/ratio_feature.py
@@ -1,4 +1,5 @@
 from backtester.features.feature import Feature
+import numpy as np
 
 
 class RatioMarketFeature(Feature):
@@ -7,19 +8,13 @@ class RatioMarketFeature(Feature):
     Computing for Instrument. By default defers to computeForLookbackData
     '''
     @classmethod
-    def computeForInstrument(cls, featureParams, featureKey, currentFeatures, instrument, instrumentManager):
-        feature1 = featureParams['featureName1']
-        feature2 = featureParams['featureName2']
-        if (instrument):
-            return 0
-        lookbackData = instrument.getDataDf()
-        if len(lookbackData) < 1:
-            return 1
-        feature1 = lookbackData[featureParams['featureName1']].iloc[-1]
-        feature2 = lookbackData[featureParams['featureName2']].iloc[-1]
-        if feature2 == 0:
-            return 0
-        return feature2 / float(feature1)
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        feature1 = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['featureName1']).iloc[-1]
+        feature2 = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['featureName2']).iloc[-1]
+
+        toRtn = feature1 / feature2
+        toRtn[toRtn == np.Inf] = 0
 
     '''
     Computing for Market. By default defers to computeForLookbackData
@@ -34,8 +29,11 @@ class RatioMarketFeature(Feature):
         if (instrument1 is None) or (instrument2 is None):
             return 0
         else:
-            instrument1Price = instrument1.getDataDf()[feature].iloc[-1]
-            instrument2Price = instrument2.getDataDf()[feature].iloc[-1]
+            instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+            dataDf = instrumentLookbackData.getDataForFeatureForAllInstruments(feature)
+
+            instrument1Price = dataDf[instrumentId1].iloc[-1]
+            instrument2Price = dataDf[instrumentId2].iloc[-1]
             if instrument2Price == 0:
                 return 0
             return instrument1Price / float(instrument2Price)
