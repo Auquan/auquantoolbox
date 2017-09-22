614f41d012abedeee3778121bcfbb927d5f1cc2f
Kanav Arora <kanav@stuph.co>
2017-09-23 03:34:19 +0530

    moving sum

--- backtester/features/moving_sum.py
+++ backtester/features/moving_sum.py
@@ -1,13 +1,16 @@
 from backtester.features.feature import Feature
-from backtester.financial_fn import ma
-import numpy as np
 
 
 class MovingSumFeature(Feature):
 
     @classmethod
-    def computeForLookbackData(cls, featureParams, featureKey, currentFeatures, lookbackDataDf):
-        data = lookbackDataDf[featureParams['featureName']]
-        #avg = ma(data, featureParams['period'])
-        return data[-featureParams['period']:].sum()
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        dataDf = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['featureName'])
+        return dataDf[-featureParams['period']:].sum()
 
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        lookbackDataDf = instrumentManager.getDataDf()
+        data = lookbackDataDf[featureParams['featureName']]
+        return data[-featureParams['period']:].sum()
