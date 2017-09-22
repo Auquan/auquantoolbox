8265cc7c021476b55224b7cbcdbd1c225fa21cb0
Kanav Arora <kanav@stuph.co>
2017-09-23 03:29:17 +0530

    moving correlation

--- backtester/features/moving_correlation.py
+++ backtester/features/moving_correlation.py
@@ -1,20 +1,17 @@
 from backtester.features.feature import Feature
-from backtester.financial_fn import ma
-import numpy as np
+
 
 # Correlation between two instruments over some number of data points specified by user.
 
 class MovingCorrelationFeature(Feature):
 
     @classmethod
-    def computeForInstrument(cls, featureParams, featureKey, currentFeatures, instrument, instrumentManager):
-        lookbackInstrumentFeaturesDf = instrument.getDataDf()
-        x = lookbackInstrumentFeaturesDf[featureParams['series1']]
-        y = lookbackInstrumentFeaturesDf[featureParams['series2']]
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        x = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['series1'])
+        y = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['series2'])
 
-        if (len(x) < 1) or (len(y) < 1):
-        	return 0
-        return round(x.rolling(featureParams['period']).corr(y)[-1], 3)
+        return (x.rolling(featureParams['period']).corr(y)).iloc[-1]
 
     @classmethod
     def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
@@ -23,5 +20,5 @@ class MovingCorrelationFeature(Feature):
         y = lookbackMarketFeaturesDf[featureParams['series2']]
 
         if (len(x) < 1) or (len(y) < 1):
-        	return 0
-        return round(x.rolling(featureParams['period']).corr(y)[-1], 3)
+            return 0
+        return round((x.rolling(featureParams['period']).corr(y)).iloc[-1], 3)
