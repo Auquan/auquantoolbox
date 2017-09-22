6daf7f40d1341523304e7b66416d11df42acd773
Kanav Arora <kanav@stuph.co>
2017-09-23 03:33:15 +0530

    moving max/min feature

--- backtester/features/moving_min.py
+++ backtester/features/moving_min.py
@@ -1,12 +1,16 @@
 from backtester.features.feature import Feature
-from backtester.financial_fn import ma
-import numpy as np
 
 
 class MovingMinimumFeature(Feature):
 
     @classmethod
-    def computeForLookbackData(cls, featureParams, featureKey, currentFeatures, lookbackDataDf):
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        dataDf = instrumentLookbackData.getDataForFeatureForAllInstruments(featureParams['featureName'])
+        return dataDf[-featureParams['period']:].min()
+
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        lookbackDataDf = instrumentManager.getDataDf()
         data = lookbackDataDf[featureParams['featureName']]
-        #avg = ma(data, featureParams['period'])
         return data[-featureParams['period']:].min()
