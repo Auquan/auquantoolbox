15f6b99faa00bc17180959aed14fe8946ae83cad
Mayank Kumar <mk9990143266@gmail.com>
2017-09-23 20:54:53 +0530

    rsi changed

--- backtester/features/rsi_feature.py
+++ backtester/features/rsi_feature.py
@@ -12,8 +12,8 @@ class RSIFeature(Feature):
         data_downside = data_upside.copy()
         data_downside[data_upside > 0] = 0
         data_upside[data_upside < 0] = 0
-        avg_upside = ma(data_upside, featureParams['period'])[-1]
-        avg_downside = - ma(data_downside, featureParams['period'])[-1]
+        avg_upside = data_upside[-featureParams['period']:].mean()
+        avg_downside = - data_downside[-featureParams['period']:].mean()
         rs = avg_upside / avg_downside
 
         return 100 - (100 * avg_downside / (avg_downside + avg_upside))
@@ -27,8 +27,8 @@ class RSIFeature(Feature):
         data_downside[data_upside > 0] = 0
         data_upside[data_upside < 0] = 0
         if len(data.index) > 0:
-            avg_upside = ma(data_upside, featureParams['period'])[-1]
-            avg_downside = - ma(data_downside, featureParams['period'])[-1]
+            avg_upside = data_upside[-featureParams['period']:].mean()
+            avg_downside = - data_downside[-featureParams['period']:].mean()
             rs = avg_upside / avg_downside
         else:
             return 0
