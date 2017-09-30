1c0c67322b9a3aab310a7a47baaec4d314b577fb
Shub Jain <Shub Jain>
2017-09-30 12:38:22 -0700

    Fix rsi

--- backtester/features/rsi_feature.py
+++ backtester/features/rsi_feature.py
@@ -14,9 +14,11 @@ class RSIFeature(Feature):
         data_upside[data_upside < 0] = 0
         avg_upside = data_upside[-featureParams['period']:].mean()
         avg_downside = - data_downside[-featureParams['period']:].mean()
-        rs = avg_upside / avg_downside
+        rsi = 100 - (100 * avg_downside / (avg_downside + avg_upside))
+        rsi[(avg_downside == 0)] = 100
+        rsi[(avg_downside == 0) & (avg_upside == 0)] = 0
 
-        return 100 - (100 * avg_downside / (avg_downside + avg_upside))
+        return rsi
 
     @classmethod
     def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
@@ -29,8 +31,10 @@ class RSIFeature(Feature):
         if len(data.index) > 0:
             avg_upside = data_upside[-featureParams['period']:].mean()
             avg_downside = - data_downside[-featureParams['period']:].mean()
-            rs = avg_upside / avg_downside
         else:
             return 0
+        rsi = 100 - (100 * avg_downside / (avg_downside + avg_upside))
+        rsi = 100 if (avg_downside == 0) else rsi
+        rsi = 0 if ((avg_downside == 0) & (avg_upside == 0)) else rsi
 
-        return 100 - (100 * avg_downside / (avg_downside + avg_upside))
+        return rsi
