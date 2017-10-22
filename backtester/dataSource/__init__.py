1cbbbd0653aff49a73ecea6746027b4b2f18bd7b
Kanav Arora <kanav@stuph.co>
2017-10-23 04:04:09 +0530

    adding new files in init.py

--- backtester/dataSource/__init__.py
+++ backtester/dataSource/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ["auquan_data_source", "data_source_utils", "data_source", "google_data_source", "logfile_data_source", "nse_data_source",
-           "quant_quest_data_source", "yahoo_data_source"]
+           "quant_quest_data_source", "yahoo_data_source", "future_role_data_source", "quandl_data_source"]
