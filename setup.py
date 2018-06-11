8b30d34cebd4d522ff1c5747526f094a8f2e1283
Shub Jain <shubham1217@gmail.com>
2018-06-11 23:55:47 +0530

    Update dependencies

--- setup.py
+++ setup.py
@@ -62,5 +62,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['plotly', 'numpy', 'pandas', 'pandas-datareader', 'beautifulsoup4']
+    install_requires=['requests', 'plotly', 'numpy', 'pandas', 'bs4', 'scipy']
 )
