56f2acc686bfb3457ebf1007d069b27c61502526
Shub Jain <shubham1217@gmail.com>
2018-07-01 00:51:09 +0530

    Fix time rule

--- backtester/timeRule/custom_time_rule.py
+++ backtester/timeRule/custom_time_rule.py
@@ -29,12 +29,24 @@ class CustomTimeRule(TimeRule):
         return pd.date_range(start=self.__startDate, end=self.__endDate, freq= self.__bhour)
 
     def createBusinessMinSeries(self):
-        hour_series = self.createBusinessHourSeries()
-        return pd.date_range(hour_series.min(), hour_series.max(), freq= self.__sample + ' min')
+        day_series = self.createBusinessDaySeries()
+        datetime_index = None
+        for day in day_series:
+            if(datetime_index is None):
+                datetime_index = pd.date_range(start=day+timedelta(minutes=555), end=day+timedelta(minutes=930), freq= self.__sample + ' min')
+            else:
+                datetime_index = datetime_index.append(pd.date_range(start=day+timedelta(minutes=555), end=day+timedelta(minutes=930), freq= self.__sample + ' min'))
+        return datetime_index
 
     def createBusinessSecSeries(self):
-        hour_series = self.createBusinessHourSeries()
-        return pd.date_range(hour_series.min(), hour_series.max(), freq= self.__sample + ' s')
+        day_series = self.createBusinessDaySeries()
+        datetime_index = None
+        for day in day_series:
+            if(datetime_index is None):
+                datetime_index = pd.date_range(start=day+timedelta(minutes=555), end=day+timedelta(minutes=930), freq= self.__sample + ' s')
+            else:
+                datetime_index = datetime_index.append(pd.date_range(start=day+timedelta(minutes=555), end=day+timedelta(minutes=930), freq= self.__sample + ' s'))
+        return datetime_index
 
     def emitTimeToTrade(self):
         time_range = None
