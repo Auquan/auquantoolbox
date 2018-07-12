f99113b82eacdaaa8e58c816e105dc528f922676
Vaibhav Nagar <vaibhavn@iitk.ac.in>
2018-07-12 19:25:52 +0530

    fix ml and backtest system

--- backtester/ml_and_backtest_system.py
+++ backtester/ml_and_backtest_system.py
@@ -9,13 +9,14 @@ except ImportError:
         import pickle
 from backtester.model_learning_system import ModelLearningSystem
 from backtester.trading_system import TradingSystem
+from backtester.features.feature import Feature
 
 class MLandBacktestSystem(object):
 
     def __init__(self, tsParams, mlsParams):
         self.tsParams = tsParams
         self.mlsParams = mlsParams
-        self.model = {}
+        self.models = {}
         self.selectedFeatures = {}
         self.transformers = {}
 
@@ -23,6 +24,8 @@ class MLandBacktestSystem(object):
         mlSystem = ModelLearningSystem(self.mlsParams, chunkSize=None)
         mlSystem.runModels()
         self.loadModels(self.tsParams.getTargetVariableKey())
+        self.setUpMLSTrainingPredictionFeature()
+
         print('Loaded Problem 1 Params, Loading Backtester and Data')
         tradingSystem = TradingSystem(self.tsParams)
         print('Loaded Backtester and Data Loaded, Backtesting')
@@ -36,22 +39,17 @@ class MLandBacktestSystem(object):
             fileName = os.path.join(self.mlsParams.modelDir, instrumentId + '_' + targetVariableKey + '.pkl')
             with open(fileName, 'rb') as f:
                 modelData = pickle.load(f)
-                # Model which has methods - fit, predict, reTrain and evaluate
-                self.model[instrumentId] = modelData.getModel()
+                # Models which has methods - fit, predict, reTrain and evaluate
+                self.models[instrumentId] = modelData.getModel()
                 # selectFeatures is the list of important feature keys
                 self.selectedFeatures[instrumentId] = modelData.getModelFeatures()
                 # transformers is the ordered dictionary of scalers or transformers
                 self.transformers[instrumentId] = modelData.getModelTransformers()
-            # self.trainingModelManager.addModel(self.model[instrumentId], instrumentId)
 
-    def transformData(self, instrumentId, X):
-        transformers = self.transformers.get(instrumentId, None)
-        if transformers is None:
-            return X
-        transformers = transformers.values if isinstance(transformers, dict) else transformers
-        for transformer in transformers:
-            X = transformer.transform(X)
-        return X
+    def setUpMLSTrainingPredictionFeature(self):
+        MLSTrainingPredictionFeature.SELECTED_FEATURES = self.selectedFeatures
+        MLSTrainingPredictionFeature.TRANSFORMERS = self.transformers
+        MLSTrainingPredictionFeature.MODELS = self.models
 
     def getPrediction(self, time, updateNum, instrumentManager, predictions):
         # holder for all the instrument features for all instruments
@@ -62,5 +60,34 @@ class MLandBacktestSystem(object):
                 X.append(lookbackInstrumentFeatures.getFeatureDf(feature)[instrumentId].iloc[-1].values)
             X = np.array(X)
             X = self.transformData(instrumentId, X)
-            predictions[instrumentId] = self.model[instrumentId].predict(X)
+            predictions[instrumentId] = self.models[instrumentId].predict(X)
+        return predictions
+
+class MLSTrainingPredictionFeature(Feature):
+    SELECTED_FEATURES = {}
+    TRANSFORMERS = {}
+    MODELS = {}
+
+    @classmethod
+    def transformData(cls, instrumentId, X):
+        transformers = cls.TRANSFORMERS.get(instrumentId, None)
+        if transformers is None:
+            return X
+        transformers = transformers.values if isinstance(transformers, dict) else transformers
+        for transformer in transformers:
+            X = transformer.transform(X)
+        return X
+
+    @classmethod
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        predictions = pd.Series(0.5, index = instrumentManager.getAllInstrumentsByInstrumentId())
+        # holder for all the instrument features for all instruments
+        lookbackInstrumentFeatures = instrumentManager.getLookbackInstrumentFeatures()
+        for instrumentId in instrumentManager.getAllInstrumentsByInstrumentId():
+            X = []
+            for feature in cls.SELECTED_FEATURES[instrumentId]:
+                X.append(lookbackInstrumentFeatures.getFeatureDf(feature)[instrumentId].iloc[-1].values)
+            X = np.array(X)
+            X = cls.transformData(instrumentId, X)
+            predictions[instrumentId] = cls.MODELS[instrumentId].predict(X)
         return predictions
