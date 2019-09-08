de59440b17cc382c935069618b4c741f6db19c2a
Chandini Jain <chandini@auquan.com>
2019-09-08 19:00:01 +0100

    fixed for no score

--- backtester/metrics/metrics_logger.py
+++ backtester/metrics/metrics_logger.py
@@ -8,6 +8,7 @@ from backtester.state_writer import StateWriter
 from tensorboardX import SummaryWriter
 from datetime import datetime
 from backtester.metrics.metrics import Metrics
+import os
 
 class MetricsLogger():
 	def __init__(self, metrics_to_log_realtime, instrumentManager, priceFeatureKey, startingCapital):
@@ -25,7 +26,7 @@ class MetricsLogger():
 		self.startingCapital = startingCapital
 
 		self.stateWriter = StateWriter('runLogs', datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S'))
-		self.tensorboard_writer = SummaryWriter(logdir='tb_logs\\'+datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S'))
+		self.tensorboard_writer = SummaryWriter(logdir=os.path.join('tb_logs', datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')))
 
 		if self.metrics_to_log_realtime['market'] is not None:
 			print('For market, only logging: %s in tensorboard'%self.metrics_to_log_realtime['market'])
@@ -53,7 +54,9 @@ class MetricsLogger():
 			stats = metrics.getMetrics()
 			# metricString = metrics.getInstrumentMetricsString()
 			resultDict['instrument_names'] += [instrumentId]
-			resultDict['instrument_stats'] += [{'total_pnl': stats['Total Pnl(%)'], 'score': stats['Score']}]
+			resultDict['instrument_stats'] += [{'total_pnl': stats['Total Pnl(%)']}]
+			if 'Score' in stats:
+				resultDict['instrument_stats'][-1]['score'] = stats['Score']
 			if 'Normalized Score' in stats:
 				resultDict['instrument_stats'][-1]['normalized_score'] = stats['Normalized Score']
 		metrics = Metrics(marketFeaturesDf=self.instrumentManager.getDataDf())
@@ -74,13 +77,15 @@ class MetricsLogger():
 		
 		market_metrics_to_show, instrument_metrics_to_show = self.metrics_to_log_realtime['market'], self.metrics_to_log_realtime['instruments']
 		market_stats = metrics.calculateMarketMetricsRealtime(marketFeaturesDf, startingCapital, market_metrics_to_show)
+		
 		instrument_stats = metrics.calculateInstrumentFeatureMetricsRealtime(instrumentIds, instrumentLookbackData, startingCapital, instrument_metrics_to_show)
 
 		portfolio_value = marketFeaturesDf['portfolio_value'][-1]  # TODO: find a better way to get this value
 		capital = marketFeaturesDf['capital'][-1]  # TODO: find a better way to get this value
-		score = marketFeaturesDf['score'][-1]
 		self.tensorboard_writer.add_scalars('capital_and_portfolio', {'capital': capital, 'portfolio_value': portfolio_value}, global_step)
-		self.tensorboard_writer.add_scalar('score', score, global_step)
+		if 'score' in marketFeaturesDf.columns:
+			score = marketFeaturesDf['score'][-1]
+			self.tensorboard_writer.add_scalar('score', score, global_step)
 
 		for scalar in market_stats.keys():
 			val = market_stats[scalar]
