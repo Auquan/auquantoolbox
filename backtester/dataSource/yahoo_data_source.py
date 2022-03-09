6dd8b466d5b015c04230df946773f335f9ff6390
mhawry <1245767+mhawry@users.noreply.github.com>
2022-03-09 11:51:27 -0500

    Fix for cached Yahoo filenames to use end date

--- backtester/dataSource/yahoo_data_source.py
+++ backtester/dataSource/yahoo_data_source.py
@@ -109,7 +109,7 @@ class InstrumentsFromFile():
 class YahooStockDataSource(DataSource):
     def __init__(self, cachedFolderName, dataSetId, instrumentIds, startDateStr, endDateStr, event='history', adjustPrice=False, downloadId=".NS", liveUpdates=True, pad=True):
         super(YahooStockDataSource, self).__init__(cachedFolderName, dataSetId, instrumentIds, startDateStr, endDateStr)
-        self.__dateAppend = "_%sto%s"%(datetime.strptime(startDateStr, '%Y/%m/%d').strftime('%Y-%m-%d'),datetime.strptime(startDateStr, '%Y/%m/%d').strftime('%Y-%m-%d'))
+        self.__dateAppend = "_%sto%s"%(datetime.strptime(startDateStr, '%Y/%m/%d').strftime('%Y-%m-%d'),datetime.strptime(endDateStr, '%Y/%m/%d').strftime('%Y-%m-%d'))
         self.__downloadId = downloadId
         self.__bookDataByFeature = {}
         self.__adjustPrice = adjustPrice
